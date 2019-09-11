d00cb67ada9e4786ba7c89de128edaa93218c647
peterdamicone <peter.damicone@gmail.com>
2019-09-11 13:46:44 -0500

    resetting to origional cli file3

--- cli.js
+++ cli.js
@@ -5,30 +5,29 @@ const envelope = require('./envelope')
 const programToRun = process.argv[2]
 
 if (programToRun === 'envelope') {
-	envelopePrompts().then((responses) => {
-		request('http://localhost:3000').then(data => {
-			envelope.createEnvelopes(responses, JSON.parse(data))
-		})
-	})
+    envelopePrompts().then((responses) => {
+        request('http://localhost:3000').then(data => {
+            envelope.createEnvelopes(responses, JSON.parse(data))
+        })
+    })
 } else if (programToRun === 'letter') {
-	console.log('letter program has not been implmented yet')
+    console.log('letter program has not been implmented yet')
 } else {
-	console.log('That is not a program yet!')
+    console.log('That is not a program yet!')
 }
 
 function envelopePrompts() {
-	const questions = [
-		{
-		  type: 'number',
-		  name: 'ageMax',
-		  message: 'age maximum'
-		},
-		{
-		  type: 'text',
-		  name: 'sex',
-		  message: 'What sexes?'
-		}
-	  ];
+    const questions = [{
+            type: 'number',
+            name: 'ageMax',
+            message: 'age maximum'
+        },
+        {
+            type: 'text',
+            name: 'sex',
+            message: 'What sexes?'
+        }
+    ];
 
-	  return prompts(questions)
+    return prompts(questions)
 }
